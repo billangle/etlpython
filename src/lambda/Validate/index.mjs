b2613f287aa660f2de6a91c0580d1f913b13ff99
William Beckett <williambeckett@Williams-MacBook-Air.local>
2025-12-30 10:51:14 -0500

    update for region support

--- src/lambda/Validate/index.mjs
+++ src/lambda/Validate/index.mjs
@@ -1,38 +1,22 @@
 
 import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
-//import {SSMClient, GetParameterCommand} from "@aws-sdk/client-ssm";
-const s3 = new S3Client({});
-//const ssmClient = new SSMClient({region: process.env.AWS_REGION});
+
 
 export const handler = async (event) => {
 
-    /*
-  let bucketName="";
-     const input = {
-        Name: "fpacFsaLandingBucketSSMName"
-    }
-        */
 
     const envBucket = process.env.LANDING_BUCKET;
     const project = process.env.PROJECT;
+    const bucketRegion = process.env.BUCKET_REGION;
 
+    const s3 = new S3Client({ region: bucketRegion });
     console.log ("Bucket: " + envBucket + " project: " + project );
-/*
-    const command = new GetParameterCommand(input);
 
-    try {
-      const res = await ssmClient.send(command);
-      bucketName = res.Parameter.Value;
+    const bucket = envBucket;
+    const prefix = `${project}/dbo`;
+    const res = await s3.send(new ListObjectsV2Command({ Bucket: bucket, Prefix: prefix, MaxKeys: 1 }));
+    const ok = (res.KeyCount ?? 0) > 0;
+    if (!ok) throw new Error(`No input objects found in s3://${bucket}/${project}/${prefix}. Upload a file and retry.`);
 
-    } catch (e) {
-        console.error ("ERROR on SSM: " + e);
-    }
-
-*/
-  const bucket = envBucket;
-  const prefix = `${project}/dbo`;
-  const res = await s3.send(new ListObjectsV2Command({ Bucket: bucket, Prefix: prefix, MaxKeys: 1 }));
-  const ok = (res.KeyCount ?? 0) > 0;
-  if (!ok) throw new Error(`No input objects found in s3://${bucket}/${project}/${prefix}. Upload a file and retry.`);
   return { ok, bucket, bucket, prefix, envBucket};
 };

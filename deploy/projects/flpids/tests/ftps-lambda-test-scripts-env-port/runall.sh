d80b084f58b2bf921d6365e0b0315bbfa955f522
William Beckett <williambeckett@Williams-MacBook-Air.local>
2026-01-14 10:47:51 -0500

    added sleep

--- deploy/projects/flpids/tests/ftps-lambda-test-scripts-env-port/runall.sh
+++ deploy/projects/flpids/tests/ftps-lambda-test-scripts-env-port/runall.sh
@@ -2,9 +2,15 @@
 
 #python3 deploy.py --config config/flpidssteam.json --region us-east-1 --project-type flpids
 ./01_gls_generic_ods.sh
+sleep 1
 ./02_plas_rc540_weekly.sh
+sleep 1
 ./03_plas_rc540_monthly.sh
+sleep 1
 ./04_plas_scims_rpt.sh
+sleep 1
 ./05_plas_mfo900_weekly_timestamped.sh
+sleep 1
 ./06_nats_csv_with_date.sh
+sleep 1
 ./07_plas_weekly_subfolder_example.sh

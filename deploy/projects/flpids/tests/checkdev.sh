2a71d7ac4e7a3fd81d5fbe0e6633a02367eeb54d
William Beckett <williambeckett@Williams-MacBook-Air.local>
2026-01-28 09:49:04 -0500

    update for insecure test

--- deploy/projects/flpids/tests/checkdev.sh
+++ deploy/projects/flpids/tests/checkdev.sh
@@ -13,6 +13,8 @@ aws lambda invoke \
     "header": false,
     "jenkins_url": "https://10.219.30.23/job/BuildProcess/job/TestFlpidsHook/build?token=72305AD4-A19D-462C-9CEB-275E01BDFAFD",
     "jenkins_tls_mode": "insecure",
+    "jenkins_login": "28722025092407116016427",
+    "jenkins_password": "11895b798e52daaad50bc841a07484faea",
     "secret_id": "FSA-CERT-Secrets",
     "ftps_mode": "explicit",
     "ftps_port": 990

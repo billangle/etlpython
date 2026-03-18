e48ce888185b352f27cca463fa85c6d364955a45
William Beckett <william.beckett@usda.gov>
2026-03-18 09:54:02 -0400

    updates for CPS config

--- deploy/projects/cps/lambda/RAW-DM-etl-workflow-update/lambda-function.py
+++ deploy/projects/cps/lambda/RAW-DM-etl-workflow-update/lambda-function.py
@@ -1,101 +1,67 @@
-'''
-Function to publish errors incase of any glue job failed .
-'''
-
-
 import json
 import boto3
 import os
-from sns_topics import sns_topics
+import psycopg2
+from datetime import datetime
 
-
+SECRET_NAME = "FSA-DEV-secrets"
 REGION = os.getenv("AWS_REGION")
-#SNS_ARN = "arn:aws:sns:us-east-1:241533156429:FSA-DEV-CPS"
-
 
 def lambda_handler(event, context):
-    # parse, format and return a single error message
-    def msg_formatter(content):
-        cause_json = json.loads(content)
-        # table_name = cause_json['Arguments']['--TableName']
-        cause_info = (f"Error Number: {counter} \n"
-                      f"JobName: {cause_json.get('JobName', 'Unknown')} \n"
-                      f"TableName: {cause_json.get('Arguments', {}).get('--TableName', 'Unknown')} \n"
-                      f"JobRunState: {cause_json.get('JobRunState', 'Unknown')} \n"
-                      f"StartedOn: {cause_json.get('StartedOn', 'Unknown')} \n"
-                      f"ErrorMessage: {cause_json.get('ErrorMessage', 'Unknown')} \n"
-                      "---------------------------------- \n")
-        return cause_info
 
+    # get secrets
+    secrets = boto3.client("secretsmanager", region_name=REGION)
+    secret_value = secrets.get_secret_value(SecretId=SECRET_NAME)
+    secret = json.loads(secret_value["SecretString"])
     
-    def check_for_input_errors(input_dict, all_fails, counter):
-        
-        for key, val in input_dict.items():
-            if isinstance(val, (int, float)):
-                pass
-            
-            elif isinstance(val, dict):
-                for item in [input_dict[key]]:
-                    if "Cause" in item:
-                        cause = item["Cause"]
-                        print(cause)
-                        message = msg_formatter(cause)
-                        all_fails += message
-                        counter += 1
-            else:
-                for item in input_dict[key]:
-                    if "Cause" in item:
-                        cause = item["Cause"]
-                        print(cause)
-                        message = msg_formatter(cause)
-                        all_fails += message
-                        counter += 1
-            
-        return all_fails
-        
-    error = event.get('Error')
-    print(f"error: {error}")
-    error_json = json.loads(error['Cause'])
-    print(f"error_json: {error_json}")
-    target = error_json["Arguments"]["--target_prefix"]
-    print(f"target: {target}")
-    env = error_json["Arguments"]["--env"]
-    print(f"env: {env}")
-
-    # iterate over input to extract and process all errors    
-    counter = 1
-    #sf_name = event.get('SfName', '')
-    sf_name = 'FSA-DEV-CPS-S3Landing-to-S3Final-Raw-DM'
-    all_fails = ''
-
-    if isinstance(event, list):
-        for input in event:
-            if isinstance(input, dict):
-                all_fails = check_for_input_errors(input, all_fails, counter)
+    # configure db connection creds
+    db_host = secret["edv_postgres_hostname"]
+    db_port = secret["postgres_port"]
+    db_name = secret["postgres_prcs_ctrl_dbname"]
+    db_user = secret["edv_postgres_username"]
+    db_password = secret["edv_postgres_password"]
     
+    conn = psycopg2.connect(
+        host=db_host,
+        port=db_port,
+        user=db_user,
+        password=db_password,
+        dbname=db_name,
+        sslmode="require"
+    )
+    
+    conn.autocommit=True
+    cursor = conn.cursor()
+
+    data_ppln_job_id = event["JobId"]
+    
+    
+    if "JobId" in event: # and 'ExecutedVersion' in event["Payload"]:
+        job_stat_nm = 'In Progress'
+        data_ppln_job_id = event["JobId"]
+        err_msg_txt = ''
     else:
-        all_fails = check_for_input_errors(event, all_fails, counter)
+        job_stat_nm = 'Failed'
+        data_ppln_job_id = event["JobId"]
+        err_msg_txt = 'errors occured when executing state machine: FSA-DEV-CPS-Step2' 
 
-    print(f"All Fails: {all_fails}")  
-                     
-    #Assign SNS Topic
-    SNS_ARN = sns_topics[target.upper()].format(env.upper())
-    print("SNS_ARN: " + SNS_ARN)
-    # if there were any failures in the preceeding steps send sns notification and fail state function
-    if all_fails:
+   
+    # update data pipeline job
+    data_ppln_job_update_on_complete_sql = f"""
+        update dart_process_control.data_ppln_job
+        set job_end_dt = %s,
+            last_cplt_data_load_tgt_nm = %s,
+            job_stat_nm = %s,err_msg_txt = %s
+        where data_ppln_job_id = %s
+        """
         
-        # concatenate the final sns message
-        sns_msg = f'One or multiple errors occured when executing state machine: {sf_name}. \n\n' + all_fails
+    cursor.execute(data_ppln_job_update_on_complete_sql, (  datetime.utcnow(), 
+                                                            "FSA-DEV-CPS",
+                                                            job_stat_nm,err_msg_txt,
+                                                            data_ppln_job_id))
+                                            
+    conn.close()
+    
 
-        sns_client = boto3.client('sns', region_name=REGION)
-        response = sns_client.publish(
-            TopicArn = SNS_ARN,
-            Message = sns_msg,
-            Subject= 'FSA-DEV-CPS-RAW-DM-NOTIFICATIONS'
-        )
-        
-        
-        #raise Exception(sns_msg)
+    return 
 
-    else:
-        return
--- deploy/projects/cps/lambda/RAW-DM-sns-pub-step-func-errs/lambda-function.py
+++ deploy/projects/cps/lambda/RAW-DM-sns-pub-step-func-errs/lambda-function.py
@@ -6,10 +6,11 @@ Function to publish errors incase of any glue job failed .
 import json
 import boto3
 import os
+from sns_topics import sns_topics
 
 
 REGION = os.getenv("AWS_REGION")
-SNS_ARN = os.environ["SNS_ARN"]
+#SNS_ARN = "arn:aws:sns:us-east-1:241533156429:FSA-DEV-CPS"
 
 
 def lambda_handler(event, context):
@@ -26,30 +27,63 @@ def lambda_handler(event, context):
                       "---------------------------------- \n")
         return cause_info
 
+    
+    def check_for_input_errors(input_dict, all_fails, counter):
+        
+        for key, val in input_dict.items():
+            if isinstance(val, (int, float)):
+                pass
+            
+            elif isinstance(val, dict):
+                for item in [input_dict[key]]:
+                    if "Cause" in item:
+                        cause = item["Cause"]
+                        print(cause)
+                        message = msg_formatter(cause)
+                        all_fails += message
+                        counter += 1
+            else:
+                for item in input_dict[key]:
+                    if "Cause" in item:
+                        cause = item["Cause"]
+                        print(cause)
+                        message = msg_formatter(cause)
+                        all_fails += message
+                        counter += 1
+            
+        return all_fails
+        
+    error = event.get('Error')
+    print(f"error: {error}")
+    error_json = json.loads(error['Cause'])
+    print(f"error_json: {error_json}")
+    target = error_json["Arguments"]["--target_prefix"]
+    print(f"target: {target}")
+    env = error_json["Arguments"]["--env"]
+    print(f"env: {env}")
+
     # iterate over input to extract and process all errors    
     counter = 1
-    sf_name = event.get('SfName', '')
+    #sf_name = event.get('SfName', '')
+    sf_name = 'FSA-DEV-CPS-S3Landing-to-S3Final-Raw-DM'
     all_fails = ''
-    for key, val in event.items():
 
-        if isinstance(val, dict):
-            for item in [event[key]]:
-                if 'Cause' in item:
-                    cause = item["Cause"]
-                    message = msg_formatter(cause)
-                    all_fails += message
-                    counter += 1
-        else:
-            for item in event[key]:
-                if 'Cause' in item:
-                    cause = item["Cause"]
-                    message = msg_formatter(cause)
-                    all_fails += message
-                    counter += 1
+    if isinstance(event, list):
+        for input in event:
+            if isinstance(input, dict):
+                all_fails = check_for_input_errors(input, all_fails, counter)
+    
+    else:
+        all_fails = check_for_input_errors(event, all_fails, counter)
+
+    print(f"All Fails: {all_fails}")  
                      
+    #Assign SNS Topic
+    SNS_ARN = sns_topics[target.upper()].format(env.upper())
+    print("SNS_ARN: " + SNS_ARN)
     # if there were any failures in the preceeding steps send sns notification and fail state function
     if all_fails:
-    
+        
         # concatenate the final sns message
         sns_msg = f'One or multiple errors occured when executing state machine: {sf_name}. \n\n' + all_fails
 
@@ -57,12 +91,11 @@ def lambda_handler(event, context):
         response = sns_client.publish(
             TopicArn = SNS_ARN,
             Message = sns_msg,
-            Subject= 'FSA-DEV-CPS-NOTIFICATIONS'
+            Subject= 'FSA-DEV-CPS-RAW-DM-NOTIFICATIONS'
         )
         
         
-        raise Exception(sns_msg)
-    
-        
+        #raise Exception(sns_msg)
+
     else:
         return
